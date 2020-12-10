67c86b2d241ad4bbe4f52344ab0fe8ec909f7348
Justin Arensman <j.arensman@buckaroo.nl>
2020-12-10 16:41:51 +0100

    Day 4 Problem 1

---
+++ AdventOfCode2020/AdventOfCode2020.Tests/Day04Tests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+	public class Day04Tests
+	{
+		private const string TestString = @"
+ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in";
+
+		private Day Day04 { get; } = new Day04();
+
+		[Fact]
+		public void Test1()
+		{
+			var result = this.Day04.Perform1(TestString);
+			Assert.Equal("2", result);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			//var result = this.Day04.Perform2(TestString);
+			//Assert.Equal("336", result);
+		}
+	}
+}
---
+++ AdventOfCode2020/AdventOfCode2020/Day04.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+	public class Day04 : Day
+	{
+		public override string Perform1(string inputString)
+		{
+			var passportInputStrings = inputString.Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries);
+			var passports = passportInputStrings.Select(ParsePassport);
+			return passports.Count(p => p.IsValid).ToString();
+		}
+
+		public override string Perform2(string inputString)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static Passport ParsePassport(string passportString)
+		{
+			var passport = new Passport();
+			var fields = passportString.Split(new[] {Environment.NewLine, " "}, StringSplitOptions.RemoveEmptyEntries);
+			foreach (var field in fields)
+			{
+				var kvp = field.Split(':', StringSplitOptions.RemoveEmptyEntries);
+				switch (kvp[0])
+				{
+					case "byr": passport.BirthYear = kvp[1]; break;
+					case "iyr": passport.IssueYear = kvp[1]; break;
+					case "eyr": passport.ExpirationYear = kvp[1]; break;
+					case "hgt": passport.Height = kvp[1]; break;
+					case "hcl": passport.HairColor = kvp[1]; break;
+					case "ecl": passport.EyeColor = kvp[1]; break;
+					case "pid": passport.PassportId = kvp[1]; break;
+					case "cid": passport.CountryId = kvp[1]; break;
+				}
+			}
+
+			return passport;
+		}
+	}
+
+	public class Passport
+	{
+		public string BirthYear { get; set; }
+		public string IssueYear { get; set; }
+		public string ExpirationYear { get; set; }
+		public string Height { get; set; }
+		public string HairColor { get; set; }
+		public string EyeColor { get; set; }
+		public string PassportId { get; set; }
+		public string CountryId { get; set; }
+
+		public bool IsValid => !String.IsNullOrEmpty(this.BirthYear) &&
+		                       !String.IsNullOrEmpty(this.IssueYear) &&
+		                       !String.IsNullOrEmpty(this.ExpirationYear) &&
+		                       !String.IsNullOrEmpty(this.Height) &&
+		                       !String.IsNullOrEmpty(this.HairColor) &&
+		                       !String.IsNullOrEmpty(this.EyeColor) &&
+		                       !String.IsNullOrEmpty(this.PassportId);
+	}
+}
