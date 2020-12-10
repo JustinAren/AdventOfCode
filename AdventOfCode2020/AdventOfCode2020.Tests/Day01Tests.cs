e9c7834f9a02f325c6c1bfc414cf7d438957d77a
Justin Arensman <j.arensman@buckaroo.nl>
2020-12-10 12:05:12 +0100

    Added program

--- AdventOfCode2020/AdventOfCode2020.Tests/Day01Tests.cs
+++ AdventOfCode2020/AdventOfCode2020.Tests/Day01Tests.cs
@@ -4,21 +4,29 @@ namespace AdventOfCode2020.Tests
 {
 	public class Day01Tests
 	{
+		private static readonly int[] TestArray = {
+			1721,
+			979,
+			366,
+			299,
+			675,
+			1456,
+		};
+
+		private Day Day01 { get; } = new Day01();
+
 		[Fact]
 		public void Test1()
 		{
-			var testArray = new[] {
-				1721,
-				979,
-				366,
-				299,
-				675,
-				1456,
-			};
-
-			var result = Day01.Perform(testArray);
-
+			var result = this.Day01.Perform1(TestArray);
 			Assert.Equal(514579, result);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			var result = this.Day01.Perform2(TestArray);
+			Assert.Equal(241861950, result);
 		}
 	}
 }
