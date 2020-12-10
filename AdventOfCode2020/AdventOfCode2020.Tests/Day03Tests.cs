2bf226a67715df4dfbef0a24bd8f4d01f3bcc94e
Justin Arensman <j.arensman@buckaroo.nl>
2020-12-10 16:04:19 +0100

    Day 3 Problem 1

---
+++ AdventOfCode2020/AdventOfCode2020.Tests/Day03Tests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+	public class Day03Tests
+	{
+		private const string TestString = @"
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+
+		private Day Day03 { get; } = new Day03();
+
+		[Fact]
+		public void Test1()
+		{
+			var result = this.Day03.Perform1(TestString);
+			Assert.Equal("7", result);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			//var result = this.Day03.Perform2(TestString);
+			//Assert.Equal("", result);
+		}
+	}
+}
---
+++ AdventOfCode2020/AdventOfCode2020/Day03.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+	public class Day03 : Day
+	{
+		public override string Perform1(string inputString)
+		{
+			var treeCount = 0;
+			var inputArray = inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+			var position = 0;
+			var lineLength = inputArray[0].Length;
+
+			foreach (var line in inputArray)
+			{
+				position %= lineLength;
+				if (line[position] == '#') treeCount++;
+				position += 3;
+			}
+
+			return treeCount.ToString();
+		}
+
+		public override string Perform2(string inputString)
+		{
+			var treeCount = 0;
+			var inputArray = inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+
+
+			return treeCount.ToString();
+		}
+	}
+}
