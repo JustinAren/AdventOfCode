b9aff6dfea04e50e8387127261b11b9986e9543c
Justin Arensman <j.arensman@buckaroo.nl>
2020-12-13 18:23:15 +0100

    Day 6 Problem 1

---
+++ AdventOfCode2020/AdventOfCode2020.Tests/Day06Tests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+	public class Day06Tests
+	{
+		private const string TestString = @"
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+		private Day Day { get; } = new Day06();
+
+		[Fact]
+		public void Test1()
+		{
+			var result = this.Day.Perform1(TestString);
+			Assert.Equal("11", result);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			//var result = this.Day.Perform2(TestString);
+			//Assert.Equal("336", result);
+		}
+	}
+}
---
+++ AdventOfCode2020/AdventOfCode2020/Day06.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+	public class Day06 : Day
+	{
+		public override string Perform1(string inputString)
+		{
+			var inputStrings = inputString.Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries);
+			return inputStrings.Select(ParseInput).Sum(d => d.Count).ToString();
+		}
+
+		public override string Perform2(string inputString)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static Dictionary<char, int> ParseInput(string inputString)
+		{
+			var resultDictionary = new Dictionary<char, int>();
+
+			var inputs = inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+			foreach (var input in inputs)
+			{
+				foreach (var character in input)
+				{
+					if (resultDictionary.ContainsKey(character)) resultDictionary[character]++;
+					else resultDictionary.Add(character, 1);
+				}
+			}
+
+			return resultDictionary;
+		}
+	}
+}
