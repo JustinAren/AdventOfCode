5b1c7f6ac7f0f434308d839493a4db51e3202174
Justin Arensman <j.arensman@buckaroo.nl>
2020-12-16 11:29:33 +0100

    Day 10 Problem 1

---
+++ AdventOfCode2020/AdventOfCode2020.Tests/Day10Tests.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+	public class Day10Tests
+	{
+		private const string TestString1 = @"
+16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+		
+		private const string TestString2 = @"
+28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+
+		private Day Day { get; } = new Day10();
+
+		[Theory]
+		[InlineData(TestString1, 35)]
+		[InlineData(TestString2, 220)]
+		public void Test1(string inputString, ulong expected)
+		{
+			var result = this.Day.Perform1(inputString);
+			Assert.Equal(expected, result);
+		}
+		
+		[Fact]
+		public void Test2()
+		{
+			//var result = this.Day.Perform2(TestString1);
+			//Assert.Equal((ulong) 62, result);
+		}
+	}
+}
---
+++ AdventOfCode2020/AdventOfCode2020/Day10.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+	public class Day10 : Day
+	{
+		public override ulong Perform1(string inputString)
+		{
+			var inputArray = (int[])this.ParseInput(inputString);
+
+			var resultDictionary = new Dictionary<int, int>()
+			{
+				{ 1, 0 },
+				{ 2, 0 },
+				{ 3, 1 },
+			};
+
+			for (var i = 0; i < inputArray.Length; i++)
+			{
+				if (i == 0) resultDictionary[inputArray[0]]++;
+				else resultDictionary[inputArray[i] - inputArray[i - 1]]++;
+			}
+
+			return (ulong)(resultDictionary[1] * resultDictionary[3]);
+		}
+
+		public override ulong Perform2(string inputString)
+		{
+			var inputArray = (int[])this.ParseInput(inputString);
+			throw new NotImplementedException();
+		}
+
+		protected override object ParseInput(string inputString)
+		{
+			return inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).OrderBy(i => i).ToArray();
+		}
+	}
+}
