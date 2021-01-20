17f0ba7c6bf430a15e8ff9a8e729ccad26cf67ed
Justin Arensman <j.arensman@buckaroo.nl>
2021-01-20 15:54:34 +0100

    Day 13 Problem 1

---
+++ AdventOfCode2020/AdventOfCode2020.Tests/Day13Tests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+	public class Day13Tests
+	{
+		private const string TestString1 = @"
+939
+7,13,x,x,59,x,31,19";
+
+		private IDay Day { get; } = new Day13();
+
+		[Theory]
+		[InlineData(TestString1, 295)]
+		public void Test1(string inputString, ulong expected)
+		{
+			var result = this.Day.Perform1(inputString);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData(TestString1, 286)]
+		public void Test2(string inputString, ulong expected)
+		{
+			var result = this.Day.Perform2(inputString);
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ AdventOfCode2020/AdventOfCode2020/Day13.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+	public class Day13 : Day<(ulong TimeStamp, short[] BusNumbers)>
+	{
+		public override ulong Perform1(string inputString)
+		{
+			var (currentTimeStamp, busNumbers) = this.ParseInput(inputString);
+			var departureTimeStamp = UInt64.MaxValue;
+			ulong usedBusNumber = 0;
+
+			foreach (var s in busNumbers.Where(number => number > 0))
+			{
+				var busNumber = (ulong) s;
+				var nextDepartureTimeStamp = busNumber * (currentTimeStamp / busNumber + 1);
+				if (departureTimeStamp <= nextDepartureTimeStamp) continue;
+
+				departureTimeStamp = nextDepartureTimeStamp;
+				usedBusNumber = busNumber;
+			}
+
+			return (departureTimeStamp - currentTimeStamp) * usedBusNumber;
+		}
+
+		public override ulong Perform2(string inputString)
+		{
+			var (timeStamp, busNumbers) = this.ParseInput(inputString);
+			throw new NotImplementedException();
+		}
+
+		protected override (ulong TimeStamp, short[] BusNumbers) ParseInput(string inputString)
+		{
+			var rows = inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+			var timeStamp = UInt64.Parse(rows[0]);
+			var busNumbers = rows[1].Split(',').Select(s => Int16.TryParse(s, out var number) ? number : (short) -1).ToArray();
+			return (timeStamp, busNumbers);
+		}
+	}
+}
