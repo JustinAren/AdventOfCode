abb3e511ab0fa67b447fc1e919e7f287edf20629
Justin Arensman <j.arensman@buckaroo.nl>
2021-11-12 12:18:25 +0100

    Year 2015 Day 04 Problem 1

---
+++ AdventOfCode/AdventOfCode2015.Tests/Day04Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCodeBase;
+using Xunit;
+
+namespace AdventOfCode2015.Tests
+{
+	public class Day04Tests
+	{
+		private IDay Day { get; } = new Day04();
+
+		[Theory]
+		[InlineData("abcdef", 609043)]
+		[InlineData("pqrstuv", 1048970)]
+		public void Test1(string inputString, long expected)
+		{
+			var result = this.Day.Perform1(inputString);
+			Assert.Equal(expected, result);
+		}
+
+		//[Theory]
+		//[InlineData("^v", 3)]
+		//[InlineData("^>v<", 3)]
+		//[InlineData("^v^v^v^v^v", 11)]
+		//public void Test2(string inputString, long expected)
+		//{
+		//	var result = this.Day.Perform2(inputString);
+		//	Assert.Equal(expected, result);
+		//}
+	}
+}
---
+++ AdventOfCode/AdventOfCode2015/Day04.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeBase;
+
+namespace AdventOfCode2015
+{
+	public class Day04 : Day<string>
+	{
+		public override long Perform1(string inputString)
+		{
+			var secretKey = this.ParseInput(inputString);
+
+			var counter = 0L;
+
+			while (true)
+			{
+				counter++;
+				var md5 = GenerateMd5($"{secretKey}{counter}");
+				if (md5.StartsWith("00000")) return counter;
+			}
+		}
+
+		public override long Perform2(string inputString)
+		{
+			var secretKey = this.ParseInput(inputString);
+			throw new NotImplementedException();
+		}
+
+		protected override string ParseInput(string inputString)
+		{
+			return inputString;
+		}
+
+		private static string GenerateMd5(string input)
+		{
+			// Use input string to calculate MD5 hash
+			using var md5 = System.Security.Cryptography.MD5.Create();
+			var inputBytes = Encoding.UTF8.GetBytes(input);
+			var hashBytes = md5.ComputeHash(inputBytes);
+
+			// Convert the byte array to hexadecimal string
+			var sb = new StringBuilder();
+			foreach (var b in hashBytes) sb.Append(b.ToString("X2"));
+			return sb.ToString();
+		}
+	}
+}
