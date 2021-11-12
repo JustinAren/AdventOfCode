ddf7a95d4ed341e8ad624d19da774e4349100ccc
JustinAren <justinarensman@gmail.com>
2021-11-12 13:43:29 +0100

    Year 2015 Day 05 (#16)
    * Year 2015 Day 05 Problem 1
    
    * Year 2015 Day 05 Problem 2
    
    Co-authored-by: Justin Arensman <j.arensman@buckaroo.nl>

---
+++ AdventOfCode/AdventOfCode2015.Tests/Day05Tests.cs
@@ -0,0 +1,69 @@
+﻿using AdventOfCodeBase;
+using Xunit;
+
+namespace AdventOfCode2015.Tests
+{
+	public class Day05Tests
+	{
+		private IDay Day { get; } = new Day05();
+
+		private const string InputString1 = @"
+ugknbfddgicrmopn
+aaa
+jchzalrnumimnmhp
+haegwjzuvuyypxyu
+dvszwmarrgswjxmb";
+
+		private const string InputString2 = @"
+qjhvhtzxzqqjkmpb
+xxyxx
+uurcxstgmygtbstg
+ieodomkazucvgmuy";
+
+		[Theory]
+		[InlineData("ugknbfddgicrmopn", true)]
+		[InlineData("aaa", true)]
+		[InlineData("jchzalrnumimnmhp", false)]
+		[InlineData("haegwjzuvuyypxyu", false)]
+		[InlineData("dvszwmarrgswjxmb", false)]
+		[InlineData("qjhvhtzxzqqjkmpb", false)]
+		[InlineData("xxyxx", false)]
+		[InlineData("ieodomkazucvgmuy", false)]
+		public void TestIsNice1(string name, bool isNice)
+		{
+			Assert.Equal(isNice, new Name(name).IsNice1);
+		}
+
+		[Theory]
+		[InlineData(InputString1, 2)]
+		[InlineData(InputString2, 0)]
+		public void Test1(string inputString, long expected)
+		{
+			var result = this.Day.Perform1(inputString);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("qjhvhtzxzqqjkmpb", true)]
+		[InlineData("xxyxx", true)]
+		[InlineData("jchzalrnumimnmhp", false)]
+		[InlineData("haegwjzuvuyypxyu", false)]
+		[InlineData("dvszwmarrgswjxmb", false)]
+		[InlineData("ieodomkazucvgmuy", false)]
+		[InlineData("aaa", false)]
+		[InlineData("ugknbfddgicrmopn", false)]
+		public void TestIsNice2(string name, bool isNice)
+		{
+			Assert.Equal(isNice, new Name(name).IsNice2);
+		}
+
+		[Theory]
+		[InlineData(InputString1, 0)]
+		[InlineData(InputString2, 2)]
+		public void Test2(string inputString, long expected)
+		{
+			var result = this.Day.Perform2(inputString);
+			Assert.Equal(expected, result);
+		}
+	}
+}
