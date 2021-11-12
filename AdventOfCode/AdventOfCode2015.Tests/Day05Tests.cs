5d53851b3399fedad2319c7b8587c87c1762b789
Justin Arensman <j.arensman@buckaroo.nl>
2021-11-12 13:03:45 +0100

    Year 2015 Day 05 Problem 1

---
+++ AdventOfCode/AdventOfCode2015.Tests/Day05Tests.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCodeBase;
+using Xunit;
+
+namespace AdventOfCode2015.Tests
+{
+	public class Day05Tests
+	{
+		private IDay Day { get; } = new Day05();
+
+		private const string InputString = @"
+ugknbfddgicrmopn
+aaa
+jchzalrnumimnmhp
+haegwjzuvuyypxyu
+dvszwmarrgswjxmb";
+
+		[Theory]
+		[InlineData("ugknbfddgicrmopn")]
+		[InlineData("aaa")]
+		public void TestIsNice(string name)
+		{
+			Assert.True(new Name(name).IsNice);
+		}
+
+		[Theory]
+		[InlineData("jchzalrnumimnmhp")]
+		[InlineData("haegwjzuvuyypxyu")]
+		[InlineData("dvszwmarrgswjxmb")]
+		public void TestIsNaughty(string name)
+		{
+			Assert.True(new Name(name).IsNaughty);
+		}
+
+		[Theory]
+		[InlineData(InputString, 2)]
+		public void Test1(string inputString, long expected)
+		{
+			var result = this.Day.Perform1(inputString);
+			Assert.Equal(expected, result);
+		}
+
+		//[Theory]
+		//[InlineData("abcdef", 6742839)]
+		//[InlineData("pqrstuv", 5714438)]
+		//public void Test2(string inputString, long expected)
+		//{
+		//	var result = this.Day.Perform2(inputString);
+		//	Assert.Equal(expected, result);
+		//}
+	}
+}
---
+++ AdventOfCode/AdventOfCode2015/Day05.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCodeBase;
+
+namespace AdventOfCode2015
+{
+	public class Day05 : Day<IEnumerable<Name>>
+	{
+		public override long Perform1(string inputString)
+		{
+			var names = this.ParseInput(inputString);
+			return names.Count(name => name.IsNice);
+		}
+
+		public override long Perform2(string inputString)
+		{
+			var names = this.ParseInput(inputString);
+			throw new System.NotImplementedException();
+		}
+
+		protected override IEnumerable<Name> ParseInput(string inputString)
+		{
+			return inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(name => new Name(name));
+		}
+	}
+
+	public record Name(string Value)
+	{
+		private static readonly char[] Vowels = { 'a', 'e', 'i', 'o', 'u' };
+		private static readonly string[] Forbidden = { "ab", "cd", "pq", "xy" };
+		private static readonly string[] Doubles = { "aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo", "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz" };
+
+		public bool IsNice => !this.HasForbiddenStrings && this.HasDoubleCharacters && this.VowelCount > 2;
+		public bool IsNaughty => !this.IsNice;
+
+		private int VowelCount => this.Value.Count(s => Vowels.Contains(s));
+		private bool HasForbiddenStrings => Forbidden.Any(s => this.Value.Contains(s));
+		private bool HasDoubleCharacters => Doubles.Any(s => this.Value.Contains(s));
+	}
+}
