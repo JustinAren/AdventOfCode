ffde007863318dc640f167ebe6d12e0de7d22b9b
Justin Arensman <justinarensman@gmail.com>
2021-12-01 17:48:23 +0100

    Implemented 2021 boiler plate

---
+++ AdventOfCode/AdventOfCode2021.Tests/GlobalUsings.cs
@@ -0,0 +1,2 @@
+﻿global using AdventOfCodeBase;
+global using Xunit;
---
+++ AdventOfCode/AdventOfCode2021/Day01.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2021;
+
+public class Day01 : Day<string>
+{
+    public override long Perform1(string inputString) => throw new NotImplementedException();
+    public override long Perform2(string inputString) => throw new NotImplementedException();
+    protected override string ParseInput(string inputString) => throw new NotImplementedException();
+}
