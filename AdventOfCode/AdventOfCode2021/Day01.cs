669d250621f4b9bace807612102944b354343a9d
JustinAren <justinarensman@gmail.com>
2021-12-02 17:25:50 +0100

    Year 2021 Day 01 (#20)
    * Implemented 2021 boiler plate
    
    * Implemented abstract test class for 2021
    
    * Problem 1
    
    * Problem 2
    
    Co-authored-by: Justin Arensman <j.arensman@buckaroo.nl>

---
+++ AdventOfCode/AdventOfCode2021/Day01.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2021;
+
+public class Day01 : Day<List<int>>
+{
+	public override long Perform1(string inputString)
+	{
+		var heights = this.ParseInput(inputString);
+		var increments = 0;
+
+		var current = heights[0];
+
+		for (var i = 1; i < heights.Count; i++)
+		{
+			if (heights[i] > current) increments++;
+			current = heights[i];
+		}
+
+		return increments;
+	}
+
+	public override long Perform2(string inputString)
+	{
+		var heights = this.ParseInput(inputString);
+		var increments = 0;
+
+		var currentSum = heights.Take(3).Sum();
+
+		for (var i = 1; i < heights.Count - 2; i++)
+		{
+			var slidingSum = new[] { heights[i], heights[i + 1], heights[i + 2] }.Sum();
+			if (slidingSum > currentSum) increments++;
+			currentSum = slidingSum;
+		}
+
+		return increments;
+	}
+
+	protected override List<int> ParseInput(string inputString) => inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList();
+}
