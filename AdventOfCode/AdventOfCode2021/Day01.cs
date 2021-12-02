f2ca289ec21c73e26f7252e241e6d222eebbb0fe
Justin Arensman <j.arensman@buckaroo.nl>
2021-12-02 16:46:39 +0100

    Problem 1

--- AdventOfCode/AdventOfCode2021/Day01.cs
+++ AdventOfCode/AdventOfCode2021/Day01.cs
@@ -1,8 +1,27 @@
 ﻿namespace AdventOfCode2021;
 
-public class Day01 : Day<string>
+public class Day01 : Day<List<int>>
 {
-    public override long Perform1(string inputString) => throw new NotImplementedException();
-    public override long Perform2(string inputString) => throw new NotImplementedException();
-    protected override string ParseInput(string inputString) => throw new NotImplementedException();
+	public override long Perform1(string inputString)
+	{
+		var heights = this.ParseInput(inputString);
+		var current = heights[0];
+		var increments = 0;
+
+		foreach (var height in heights.Skip(1))
+		{
+			if (height > current) increments++;
+			current = height;
+		}
+
+		return increments;
+	}
+
+	public override long Perform2(string inputString) => throw new NotImplementedException();
+
+	protected override List<int> ParseInput(string inputString)
+	{
+		var result = inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList();
+		return result;
+	}
 }
