7d6f3c839cf0d0adbbbc618bf17b15fe488d6ebc
Justin Arensman <j.arensman@buckaroo.nl>
2021-12-07 15:57:14 +0100

    Added Day03 boilerplate

---
+++ AdventOfCode/AdventOfCode2021.Tests/Day03Tests.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode2021.Tests;
+
+public class Day03Tests : DayTest<Day03>
+{
+	private const string Input = @"
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
+
+	[Theory]
+	[InlineData(Input, 198)]
+	public override void Test1(string inputString, long expected)
+	{
+		var result = this.Day.Perform1(inputString);
+		Assert.Equal(expected, result);
+	}
+
+	public override void Test2(string inputString, long expected)
+	{
+		var result = this.Day.Perform2(inputString);
+		Assert.Equal(expected, result);
+	}
+}
---
+++ AdventOfCode/AdventOfCode2021/Day03.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2021;
+
+public class Day03 : Day<string>
+{
+	public override long Perform1(string inputString)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override long Perform2(string inputString)
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override string ParseInput(string inputString)
+	{
+		throw new NotImplementedException();
+	}
+}
