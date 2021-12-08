7a9fbdf9677fcc59fd6bb432eaa2b6802544a759
Justin Arensman <j.arensman@buckaroo.nl>
2021-12-08 16:13:00 +0100

    Day 03 Problem 1

--- AdventOfCode/AdventOfCode2021/Day03.cs
+++ AdventOfCode/AdventOfCode2021/Day03.cs
@@ -1,10 +1,45 @@
-﻿namespace AdventOfCode2021;
+﻿using System.Text;
 
-public class Day03 : Day<string>
+namespace AdventOfCode2021;
+
+public class Day03 : Day<string[]>
 {
 	public override long Perform1(string inputString)
 	{
-		throw new NotImplementedException();
+		var input = this.ParseInput(inputString);
+		var gammaBuilder = new StringBuilder();
+		var epsilonBuilder = new StringBuilder();
+
+		for (var i = 0; i < input[0].Length; i++)
+		{
+			var zeroCount = 0;
+			var oneCount = 0;
+
+			foreach (var row in input)
+			{
+				switch (row[i])
+				{
+					case '0': zeroCount++; break;
+					case '1': oneCount++; break;
+				}
+			}
+
+			if (zeroCount > oneCount)
+			{
+				gammaBuilder.Append('0');
+				epsilonBuilder.Append('1');
+			}
+			else
+			{
+				gammaBuilder.Append('1');
+				epsilonBuilder.Append('0');
+			}
+		}
+
+		var gamma = Convert.ToInt64(gammaBuilder.ToString(), 2);
+		var epsilon = Convert.ToInt64(epsilonBuilder.ToString(), 2);
+
+		return gamma * epsilon;
 	}
 
 	public override long Perform2(string inputString)
@@ -12,8 +47,9 @@ public class Day03 : Day<string>
 		throw new NotImplementedException();
 	}
 
-	protected override string ParseInput(string inputString)
+	protected override string[] ParseInput(string inputString)
 	{
-		throw new NotImplementedException();
+		var result = inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+		return result;
 	}
 }
