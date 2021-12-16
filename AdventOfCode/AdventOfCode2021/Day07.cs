edc66ef3c0ba55d3d6bef25c7c3472484da86416
JustinAren <justinarensman@gmail.com>
2021-12-16 17:29:41 +0100

    Year 2021 Day 07 (#26)
    * Day 07 Problem 1
    
    * Day 07 Problem 2
    
    Co-authored-by: Justin Arensman <j.arensman@buckaroo.nl>

---
+++ AdventOfCode/AdventOfCode2021/Day07.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode2021;
+
+public class Day07 : Day<long[]>
+{
+	public override long Perform1(string inputString)
+	{
+		var positions = this.ParseInput(inputString);
+		var fuelCosts = new List<long>();
+
+		foreach (var alignment in Enumerable.Range(0, (int) positions[^1]))
+		{
+			var fuelCost = positions.Sum(position => Math.Abs(position - alignment));
+			fuelCosts.Add(fuelCost);
+		}
+
+		return fuelCosts.Min();
+	}
+
+	public override long Perform2(string inputString)
+	{
+		var positions = this.ParseInput(inputString);
+
+		var fuelCosts = new List<long>();
+
+		foreach (var alignment in Enumerable.Range(0, (int) positions[^1]))
+		{
+			var fuelCost = positions.Sum(position => Enumerable.Range(1, (int) Math.Abs(position - alignment)).Sum());
+			fuelCosts.Add(fuelCost);
+		}
+
+		return fuelCosts.Min();
+	}
+
+	protected override long[] ParseInput(string inputString)
+	{
+		return inputString.Trim().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(Int64.Parse).OrderBy(x => x).ToArray();
+	}
+}
