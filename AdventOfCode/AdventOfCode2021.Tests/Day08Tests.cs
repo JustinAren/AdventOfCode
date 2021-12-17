f155e0e9add91054d8e3b7286cbf71b1c72afe6d
Justin Arensman <j.arensman@buckaroo.nl>
2021-12-17 17:13:02 +0100

    Day08 Boilerplate

---
+++ AdventOfCode/AdventOfCode2021.Tests/Day08Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode2021.Tests;
+
+public class Day08Tests : DayTest<Day08>
+{
+	private const string Input1 = @"acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+	private const string Input2 = @"
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
+
+	[Theory]
+	[InlineData(Input1, 0)]
+	[InlineData(Input2, 26)]
+	public override void Test1(string inputString, long expected)
+	{
+		var result = this.Day.Perform1(inputString);
+		Assert.Equal(expected, result);
+	}
+
+	//[Theory]
+	//[InlineData(Input1, 168)]
+	//[InlineData(Input2, 168)]
+	public override void Test2(string inputString, long expected)
+	{
+		var result = this.Day.Perform2(inputString);
+		Assert.Equal(expected, result);
+	}
+}
---
+++ AdventOfCode/AdventOfCode2021/Day08.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode2021;
+
+public class Day08 : Day<Pattern[]>
+{
+	public override long Perform1(string inputString)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override long Perform2(string inputString)
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override Pattern[] ParseInput(string inputString)
+	{
+		return inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(Pattern.Parse).ToArray();
+	}
+}
+
+public readonly record struct Signal(string Value)
+{
+	public int Length => this.Value.Length;
+}
+
+public readonly record struct Pattern(Signal[] InputSignals, Signal[] OutputSignals)
+{
+	public static Pattern Parse(string input)
+	{
+
+	}
+}
