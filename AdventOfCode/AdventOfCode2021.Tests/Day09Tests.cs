36154bd55c55ec7e2d2c7c105d1a69bc55069267
Justin Arensman <j.arensman@buckaroo.nl>
2021-12-21 16:49:02 +0100

    Day 09 Problem 1

---
+++ AdventOfCode/AdventOfCode2021.Tests/Day09Tests.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2021.Tests;
+
+public class Day09Tests : DayTest<Day09>
+{
+	private const string Input = @"
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+
+	[Theory]
+	[InlineData(Input, 15)]
+	public override void Test1(string inputString, long expected)
+	{
+		var result = this.Day.Perform1(inputString);
+		Assert.Equal(expected, result);
+	}
+
+	//[Theory]
+	//[InlineData(Input, 5353)]
+	public override void Test2(string inputString, long expected)
+	{
+		var result = this.Day.Perform2(inputString);
+		Assert.Equal(expected, result);
+	}
+}
---
+++ AdventOfCode/AdventOfCode2021/Day09.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode2021;
+
+public class Day09 : Day<long[,]>
+{
+	public override long Perform1(string inputString)
+	{
+		var grid = this.ParseInput(inputString);
+		var result = 0L;
+
+		var iMax = grid.GetLength(0);
+		var jMax = grid.GetLength(1);
+
+		for (var i = 0; i < iMax; i++)
+		{
+			for (var j = 0; j < jMax; j++)
+			{
+				var current = grid[i, j];
+
+				if (i > 0 && grid[i - 1, j] <= current) continue;
+				if (j > 0 && grid[i, j - 1] <= current) continue;
+				if (i < iMax - 1 && grid[i + 1, j] <= current) continue;
+				if (j < jMax - 1 && grid[i, j + 1] <= current) continue;
+
+				result += current + 1;
+			}
+		}
+
+		return result;
+	}
+
+	public override long Perform2(string inputString)
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override long[,] ParseInput(string inputString)
+	{
+		var rows = inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+		var result = new long[rows.Length, rows[0].Length];
+		for (var i = 0; i < rows.Length; i++)
+		for (var j = 0; j < rows[i].Length; j++)
+			result[i,j] = Int64.Parse(rows[i][j].ToString());
+		return result;
+	}
+}
