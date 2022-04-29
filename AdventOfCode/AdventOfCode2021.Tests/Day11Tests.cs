f2ca9e5d9fdd58f3aaead0a862659b8a8376a251
Justin Arensman <j.arensman@buckaroo.nl>
2022-04-29 16:31:10 +0200

    Day 11 Problem 1

---
+++ AdventOfCode/AdventOfCode2021.Tests/Day11Tests.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode2021.Tests;
+
+public class Day11Tests : DayTest<Day11>
+{
+	private const string Input = @"
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
+
+	[Theory]
+	[InlineData(Input, 1656)]
+	public override void Test1(string inputString, long expected)
+	{
+		var result = this.Day.Perform1(inputString);
+		Assert.Equal(expected, result);
+	}
+
+	[Theory]
+	[InlineData(Input, 288957)]
+	public override void Test2(string inputString, long expected)
+	{
+		//var result = this.Day.Perform2(inputString);
+		//Assert.Equal(expected, result);
+	}
+}
