e1b6cc42b8b9cf5ae37f4dda79a0325e81fd2d76
JustinAren <justinarensman@gmail.com>
2022-12-08 10:25:20 +0100

    Year 2022 Day 04 (#34)
    * Problem 1
    
    * Problem 2

---
+++ AdventOfCode/AdventOfCode2022.Tests/Day04Tests.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode2022.Tests;
+
+public class Day04Tests : DayTest<Day04>
+{
+    private const string Input = @"
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+    [Theory]
+    [InlineData(Input, 2)]
+    public override void Test1(string inputString, long expected)
+    {
+        var result = Day.Perform1(inputString);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(Input, 4)]
+    public override void Test2(string inputString, long expected)
+    {
+        var result = Day.Perform2(inputString);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AdventOfCode/AdventOfCode2022/Day04.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2022;
+
+public class Day04 : Day<AssingmentPair[]>
+{
+	public override long Perform1(string inputString)
+	{
+		var assignmentPairs = this.ParseInput(inputString);
+		return assignmentPairs.Count(pair => pair.RangesFullyOverlap);
+	}
+
+	public override long Perform2(string inputString)
+	{
+		var assignmentPairs = this.ParseInput(inputString);
+		return assignmentPairs.Count(pair => pair.RangesOverlap);
+	}
+
+	protected override AssingmentPair[] ParseInput(string inputString) => inputString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(AssingmentPair.Parse).ToArray();
+}
+
+public readonly record struct AssingmentPair(int LowerboundA, int UpperboundA, int LowerboundB, int UpperboundB)
+{
+	public bool RangesFullyOverlap =>
+		(this.LowerboundA >= this.LowerboundB && this.UpperboundA <= this.UpperboundB) ||
+		(this.LowerboundB >= this.LowerboundA && this.UpperboundB <= this.UpperboundA);
+
+	public bool RangesOverlap =>
+		(this.LowerboundA >= this.LowerboundB && this.LowerboundA <= this.UpperboundB) ||
+		(this.UpperboundA >= this.LowerboundB && this.UpperboundA <= this.UpperboundB) ||
+		(this.LowerboundB >= this.LowerboundA && this.LowerboundB <= this.UpperboundA) ||
+		(this.UpperboundB >= this.LowerboundA && this.UpperboundB <= this.UpperboundA);
+
+	public static AssingmentPair Parse(string inputString)
+	{
+		var splits = inputString.Split(",", StringSplitOptions.RemoveEmptyEntries);
+		var boundsA = splits[0].Split("-", StringSplitOptions.RemoveEmptyEntries);
+		var boundsB = splits[1].Split("-", StringSplitOptions.RemoveEmptyEntries);
+		return new AssingmentPair(int.Parse(boundsA[0]), int.Parse(boundsA[1]), int.Parse(boundsB[0]), int.Parse(boundsB[1]));
+	}
+}
