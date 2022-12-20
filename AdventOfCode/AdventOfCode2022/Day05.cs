b1a11b9543e63f40fc2a835017a154367c3136cd
Justin Arensman <justinarensman@gmail.com>
2022-12-20 11:41:12 +0100

    Solved Problem 1

--- AdventOfCode/AdventOfCode2022/Day05.cs
+++ AdventOfCode/AdventOfCode2022/Day05.cs
@@ -1,20 +1,58 @@
-﻿namespace AdventOfCode2022
+﻿using System.Text;
+
+namespace AdventOfCode2022;
+
+public class Day05 : Day<StackingPlan>
 {
-    public class Day05 : Day<int[]>
+    protected override StackingPlan ParseInput(string inputString) => StackingPlan.Parse(inputString);
+
+    public override string Perform1(string inputString)
     {
-        protected override int[] ParseInput(string inputString)
-        {
-            throw new NotImplementedException();
-        }
+        var plan = ParseInput(inputString);
+        plan.ProcessInstructions();
+        var resultBuilder = new StringBuilder();
+        foreach (var stack in plan.Stacks) resultBuilder.Append(stack.Pop());
+        return resultBuilder.ToString();
+    }
 
-        public override string Perform1(string inputString)
-        {
-            throw new NotImplementedException();
-        }
+    public override string Perform2(string inputString)
+    {
+        throw new NotImplementedException();
+    }
+}
 
-        public override string Perform2(string inputString)
-        {
-            throw new NotImplementedException();
-        }
+public record struct Instruction(int Count, int From, int To)
+{
+    public static Instruction Parse(string inputString)
+    {
+        var words = inputString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return new Instruction(int.Parse(words[1]), int.Parse(words[3]), int.Parse(words[5]));
+    }
+}
+
+public record class StackingPlan(List<Stack<char>> Stacks, List<Instruction> Instructions)
+{
+    public static StackingPlan Parse(string inputString)
+    {
+        var parts = inputString.Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries);
+        var stackLines = parts[0].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Reverse().ToList();
+        var stacks = new List<Stack<char>>();
+        for (var i = 0; i < stackLines[0].Length / 4 + 1; i++) stacks.Add(new Stack<char>());
+
+        foreach (var line in stackLines)
+            for (var i = 0; i < line.Length; i++)
+                if (line[i] >= 'A' && line[i] <= 'Z')
+                    stacks[i / 4].Push(line[i]);
+
+        var instructions = parts[1].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(Instruction.Parse).ToList();
+
+        return new StackingPlan(stacks, instructions);
+    }
+
+    public void ProcessInstructions()
+    {
+        foreach (var instruction in Instructions)
+            for (var i = 0; i < instruction.Count; i++)
+                Stacks[instruction.To- 1].Push(Stacks[instruction.From - 1].Pop());
     }
 }
