2af2a8e11efd8376986b29e2f53f160b691d11d2
Justin Arensman <justin.arensman@orcagroup.com>
2023-12-01 16:40:49 +0100

    Boilerplate for 2023

---
+++ AdventOfCode/AdventOfCode2023.Tests/Day01Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2023.Tests;
+
+public class Day01Tests : DayTest<Day01>
+{
+    private const string Input = """
+                                 1000
+                                 2000
+                                 3000
+
+                                 4000
+
+                                 5000
+                                 6000
+
+                                 7000
+                                 8000
+                                 9000
+
+                                 10000
+                                 """;
+
+    [Theory]
+    [InlineData(Input, "24000")]
+    public override void Test1(string inputString, string expected)
+    {
+        var result = Day.Perform1(inputString);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(Input, "45000")]
+    public override void Test2(string inputString, string expected)
+    {
+        var result = Day.Perform2(inputString);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AdventOfCode/AdventOfCode2023/Day01.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode2023;
+
+public class Day01 : Day<long[][]>
+{
+    protected override long[][] ParseInput(string inputString) => inputString
+        .Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries)
+        .Select(group => group.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse).ToArray())
+        .ToArray();
+
+    public override string Perform1(string inputString)
+    {
+        var groups = ParseInput(inputString);
+        return groups.Select(group => group.Sum()).Max().ToString();
+    }
+
+    public override string Perform2(string inputString)
+    {
+        var groups = ParseInput(inputString);
+        return groups.Select(group => group.Sum()).OrderByDescending(sum => sum).Take(3).Sum().ToString();
+    }
+}
