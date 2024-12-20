6908b8312f199802499b7744b58cca63dd3b4777
Justin Arensman <justin.arensman@orcagroup.com>
2024-12-20 15:25:33 +0100

    Day 02 Problem 1

---
+++ AdventOfCode/AdventOfCode2024.Tests/Day02Tests.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2024.Tests;
+
+public class Day02Tests : DayTest<Day02>
+{
+    private const string Input = """
+                                 7 6 4 2 1
+                                 1 2 7 8 9
+                                 9 7 6 2 1
+                                 1 3 2 4 5
+                                 8 6 4 4 1
+                                 1 3 6 7 9
+                                 """;
+
+    [Theory]
+    [InlineData(Input, "2")]
+    public override void Test1(string inputString, string expected)
+    {
+        var result = Day.Perform1(inputString);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(Input, "31")]
+    public override void Test2(string inputString, string expected)
+    {
+        var result = Day.Perform2(inputString);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AdventOfCode/AdventOfCode2024/Day02.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode2024;
+
+public class Day02 : Day<Report[]>
+{
+    protected override Report[] ParseInput(string inputString)
+        => inputString.SplitNewLine().Select(Report.Parse).ToArray();
+
+    public override string Perform1(string inputString)
+    {
+        var reports = ParseInput(inputString);
+        return reports.Count(report => report.IsSafe()).ToString();
+    }
+
+    public override string Perform2(string inputString)
+    {
+        var input = ParseInput(inputString);
+        throw new NotImplementedException();
+    }
+}
+
+public record Report(int[] Levels)
+{
+    public static Report Parse(string row)
+    {
+        var levels = row.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(int.Parse).ToArray();
+
+        return new Report(levels);
+    }
+
+    public bool IsSafe()
+    {
+        bool? increasing = null;
+        for (var i = 1; i < Levels.Length; i++)
+        {
+            if (Levels[i] == Levels[i - 1]) return false;
+            if (Math.Abs(Levels[i] - Levels[i - 1]) > 3) return false;
+
+            if (increasing is null) increasing = Levels[i] > Levels[i - 1];
+            else if (Levels[i] > Levels[i - 1] != increasing) return false;
+        }
+
+        return true;
+    }
+}
