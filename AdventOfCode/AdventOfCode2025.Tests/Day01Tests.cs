0e663e016a56f056755cfa827a407ef6f573a172
Justin Arensman <justinarensman@gmail.com>
2025-12-29 12:11:13 +0100

    Adds project structure for Advent of Code 2025
    Initializes the project and test projects for the Advent of Code 2025.
    
    This commit sets up the basic file structure, project references, and
    dependencies needed to start implementing solutions for the Advent of
    Code 2025 challenges.

---
+++ AdventOfCode/AdventOfCode2025.Tests/Day01Tests.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2025.Tests;
+
+public class Day01Tests : DayTest<Day01>
+{
+    public override void Test1(string inputString, string expected)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Test2(string inputString, string expected)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AdventOfCode/AdventOfCode2025/Day01.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2025;
+
+public class Day01 : Day<string>
+{
+    public override string Perform1(string inputString) => throw new NotImplementedException();
+
+    public override string Perform2(string inputString) => throw new NotImplementedException();
+
+    protected override string ParseInput(string inputString) => throw new NotImplementedException();
+}
