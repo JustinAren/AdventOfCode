58de4378c1e965a02f7c6008ee929678de95b0da
Justin Arensman <justinarensman@gmail.com>
2026-01-11 13:02:30 +0100

    Implements Day 02 puzzle for AoC 2025
    Adds the solution for Day 02 of Advent of Code 2025.
    
    This commit includes the puzzle input, parsing logic,
    and implementations for both parts of the challenge.
    Part 1 calculates a sum based on specific ID properties
    within ranges, while Part 2 remains a stub.

---
+++ AdventOfCode/AdventOfCode2025/Day02.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2025;
+
+public class Day02 : Day<List<(long First, long Last)>>
+{
+    public override string Perform1(string inputString)
+    {
+        var ranges = ParseInput(inputString);
+
+        var result = 0L;
+
+        foreach (var (first, last) in ranges)
+        {
+            for (var i = first; i <= last; i++)
+            {
+                var id = i.ToString();
+                var idHalf = id.Length / 2;
+                if (id[..idHalf] == id[idHalf..]) result += i;
+            }
+        }
+
+        return result.ToString();
+    }
+
+    public override string Perform2(string inputString)
+    {
+        var result = 0;
+        return result.ToString();
+    }
+
+    protected override List<(long First, long Last)> ParseInput(string inputString) =>
+        inputString
+            .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(row => (long.Parse(row.Split('-')[0]), long.Parse(row.Split('-')[1]))).ToList();
+}
