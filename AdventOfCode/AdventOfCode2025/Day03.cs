41cc3822a89b0e5910ebd7853e18726ffa28cbf0
JustinAren <justinarensman@gmail.com>
2026-01-16 15:38:22 +0100

    feat: solves day 3 of AoC 2025 (#48)
    * feat: implements day 03 of AoC 2025
    
    Adds the solution for day 03 of Advent of Code 2025.
    
    Calculates joltage based on bank values for part 1.
    Adds placeholder return for part 2.
    
    * feat: implements day 3 solution
    
    Implements the solution for Day 3 of Advent of Code 2025.
    
    This commit introduces a new algorithm to calculate joltage based on a bank of numbers and a given battery count. It sums the results for multiple banks.
    
    The algorithm finds the maximum value within the given range, appends it to the result, and removes the processed section from the bank.
    
    Updates the test case with the expected value for part 2.

---
+++ AdventOfCode/AdventOfCode2025/Day03.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace AdventOfCode2025;
+
+public class Day03 : Day<List<string>>
+{
+    private static long CalculateJoltage(string bank, int batteryCount)
+    {
+        var joltageSb = new StringBuilder(batteryCount);
+
+        while (batteryCount > 0)
+        {
+            var length = batteryCount == 1 ? bank.Length : (bank.Length - batteryCount) + 1;
+            var maxChar = bank[..length].Max();
+            var maxCharIndex = bank.IndexOf(maxChar);
+            joltageSb.Append(maxChar);
+            bank = bank.Remove(0, maxCharIndex + 1);
+            batteryCount--;
+        }
+
+        return long.Parse(joltageSb.ToString());
+    }
+
+    protected override List<string> ParseInput(string inputString) => inputString.SplitNewLine().ToList();
+
+    public override string Perform1(string inputString)
+    {
+        var banks = ParseInput(inputString);
+        var result = banks.Sum(bank => CalculateJoltage(bank, 2));
+        return result.ToString();
+    }
+
+    public override string Perform2(string inputString)
+    {
+        var banks = ParseInput(inputString);
+        var result = banks.Sum(bank => CalculateJoltage(bank, 12));
+        return result.ToString();
+    }
+}
--- README.md
+++ README.md
@@ -186,7 +186,7 @@
 |---:|---:|---:|
 |[1](https://adventofcode.com/2025/day/1)|980|5961|
 |[2](https://adventofcode.com/2025/day/2)|52316131093|69564213293|
-|[3](https://adventofcode.com/2025/day/3)|||
+|[3](https://adventofcode.com/2025/day/3)|17535|173577199527257|
 |[4](https://adventofcode.com/2025/day/4)|||
 |[5](https://adventofcode.com/2025/day/5)|||
 |[6](https://adventofcode.com/2025/day/6)|||
