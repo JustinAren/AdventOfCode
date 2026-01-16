6e3e93cd28051e67ccf39c7c3550e40d2f40479f
Justin Arensman <justin.arensman@orcagroup.com>
2026-01-16 14:49:10 +0100

    feat: implements day 03 of AoC 2025
    Adds the solution for day 03 of Advent of Code 2025.
    
    Calculates joltage based on bank values for part 1.
    Adds placeholder return for part 2.

---
+++ AdventOfCode/AdventOfCode2025.Tests/Day03Tests.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode2025.Tests;
+
+public class Day03Tests : DayTest<Day03>
+{
+    private const string Input =
+        """
+        987654321111111
+        811111111111119
+        234234234234278
+        818181911112111
+        """;
+
+    [Theory]
+    [InlineData(Input, "357")]
+    public override void Test1(string inputString, string expected)
+    {
+        var result = Day.Perform1(inputString);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(Input, "4174379265")]
+    public override void Test2(string inputString, string expected)
+    {
+        var result = Day.Perform2(inputString);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AdventOfCode/AdventOfCode2025/Day03.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2025;
+
+public class Day03 : Day<List<string>>
+{
+    protected override List<string> ParseInput(string inputString) => inputString.SplitNewLine().ToList();
+
+    public override string Perform1(string inputString)
+    {
+        var banks = ParseInput(inputString);
+        var result = 0L;
+
+        foreach (var bank in banks)
+        {
+            var joltage = 0;
+
+            for (var i = 0; i < bank.Length - 1; i++)
+            {
+                for (var j = i + 1; j < bank.Length; j++)
+                {
+                    var value = int.Parse($"{bank[i]}{bank[j]}");
+                    if (value > joltage) joltage = value;
+                }
+            }
+
+            result += joltage;
+        }
+
+        return result.ToString();
+    }
+
+    public override string Perform2(string inputString)
+    {
+        var ranges = ParseInput(inputString);
+        var result = 0L;
+        return result.ToString();
+    }
+}
